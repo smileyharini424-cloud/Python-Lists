d0fd9043930766f93738507f2715b7beb3b01977
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-18 22:43:25 -0700

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,11 @@
+students = ["Harini", "Anu", "Ravi", "Kiran"]
+
+print("Original List:", students)
+
+print("First Element:", students[0])
+print("Last Element:", students[-1])
+
+students[1] = "Priya"
+
+print("Modified List:", students)
+print("Sliced List:", students[:3])
